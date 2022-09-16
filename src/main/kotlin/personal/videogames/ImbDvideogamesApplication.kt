430add9cb444657a9724526408142d46d808d0ea
Leandro <leandroarielfontana.q@gmail.com>
2022-09-16 14:54:27 -0300

    Feature: Initial Commit

---
+++ src/main/kotlin/personal/videogames/ImbDvideogamesApplication.kt
@@ -0,0 +1,11 @@
+package personal.videogames
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImbDvideogamesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImbDvideogamesApplication>(*args)
+}
---
+++ src/test/kotlin/personal/videogames/ImbDvideogamesApplicationTests.kt
@@ -0,0 +1,13 @@
+package personal.videogames
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImbDvideogamesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
